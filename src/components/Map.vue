42d622dcf7ffe1be3a9e101d91b939fa9a5f71cd
Luis Romero <19170067@uttcampus.edu.mx>
2022-08-15 17:52:06 -0500

    Axios

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -19,7 +19,9 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+        
   data() {
     return {
       center: { lat: 51.093048, lng: 6.84212 },
@@ -51,6 +53,14 @@ export default {
       ],
     };
   },
+  methods: {
+   getMarkers(){
+    axios.get('http://localhost:8000/api/locations').then((res) => {
+            console.log(res.data);
+            //setposts(res.data);
+        });
+   } 
+  }
 };
 </script>
 
